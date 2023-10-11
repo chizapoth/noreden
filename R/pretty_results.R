baeea7bbf80e10011309dcbc5dd8938e4f46c44f
chizhang <andreachizhang@yahoo.com>
2023-10-11 19:52:09 +0200

    visualize results

---
+++ R/pretty_results.R
@@ -0,0 +1,286 @@
+
+# ddd <- prep_diet_comparison_gram(data_dietsummary = data_newdiet)
+
+# plot diet ----
+
+prep_diet_comparison_gram <- function(data_dietsummary){
+  
+  d <- data_dietsummary
+  # add group_macro
+  if(!"group_macro" %in% colnames(data_dietsummary)){
+    d <- dplyr::left_join(data_dietsummary,
+                          foodname_group,
+                          by = 'food_name')
+  }
+  # select
+  pd <- select(d, c(food_name, current, new, group_macro))
+  # order
+  name_ordered <- foodname_group$food_name
+  pd$food_name_ordered <- factor(pd$food_name, 
+                                 levels = name_ordered, 
+                                 labels = name_ordered)
+  pd
+  
+  result <- list(data_dietsummary = data_dietsummary, 
+                 plot_data = pd)
+  
+  # attach class attribute for s3
+  result <- structure(result, 
+                      class = 'diet_comparison_gram')
+  return(result)
+}
+
+
+
+
+
+
+# plot.diet_comparison_gram(plot_obj = ddd,
+#                           title_text = 'New diet',
+#                           axis_x_text = 'Food groups',
+#                           axis_y_text = 'Intake (grams)')
+# 
+# data_newdiet
+# 
+# plot(plot_obj = ddd,
+#      title_text = 'New diet',
+#      axis_x_text = 'Food groups',
+#      axis_y_text = 'Intake (grams)')
+
+plot.diet_comparison_gram <- function(plot_obj, 
+                                      title_text, 
+                                      axis_x_text,
+                                      axis_y_text){
+  
+
+  # need to match the column names
+  pd <- plot_obj$plot_data
+  p <- ggplot(data = pd, aes(x = food_name_ordered, 
+                             y = new, 
+                             fill = group_macro))
+  p <- p + geom_bar(position = 'dodge', 
+                    stat = 'identity')
+  p <- p + coord_flip()
+  
+  # include numbers at the end of bar
+  p <- p + geom_text(aes(label = round(new, 1)), 
+                       hjust = -0.8, 
+                       color = 'black', 
+                       size = 4)
+  p <- p + ylim(0, max(pd$new)*1.1)
+  
+  # include current diet (as vertical bars)
+  p <- p + geom_errorbar(aes(ymin = current, 
+                             ymax = current+1), 
+                           width = 1)
+  
+  # themes
+  p <- p + theme_minimal()
+  p <- p + theme(panel.grid.major.y = element_blank())
+  p <- p + scale_fill_brewer(palette = 'Dark2')
+  p <- p + labs(title = title_text, 
+                  x = axis_x_text, 
+                  y = axis_y_text)
+  p <- p + theme(axis.text = element_text(size = 12), 
+                   axis.title = element_text(size = 12), 
+                   axis.title.y = element_blank(),
+                   plot.title = element_text(size = 20), 
+                   legend.position = 'none')
+  
+  p
+  return(p)
+}
+
+
+
+ddd <- prep_diet_comparison_percent(data_dietsummary = data_newdiet)
+
+prep_diet_comparison_percent <- function(data_dietsummary){
+  
+  d <- data_dietsummary
+  # add group_macro
+  if(!"group_macro" %in% colnames(data_dietsummary)){
+    d <- dplyr::left_join(data_dietsummary,
+                          foodname_group,
+                          by = 'food_name')
+  }
+  # select
+  pd <- select(d, c(food_name, group_macro, percent_change))
+  # order
+  name_ordered <- foodname_group$food_name
+  pd$food_name_ordered <- factor(pd$food_name, 
+                                 levels = name_ordered, 
+                                 labels = name_ordered)
+  pd
+  
+  result <- list(data_dietsummary = data_dietsummary, 
+                 plot_data = pd)
+  
+  # attach class attribute for s3
+  result <- structure(result, 
+                      class = 'diet_comparison_percent')
+  return(result)
+}
+
+
+
+# plot.diet_comparison_percent(plot_obj = ddd,
+#                           title_text = 'New diet',
+#                           axis_x_text = 'Food groups',
+#                           axis_y_text = 'Intake (grams)')
+
+plot.diet_comparison_percent <- function(plot_obj, 
+                                      title_text, 
+                                      axis_x_text,
+                                      axis_y_text){
+  
+  pd <- plot_obj$plot_data
+  # need to match the column names
+  
+  p <- ggplot(data = pd, aes(x = food_name_ordered, 
+                               y = percent_change, 
+                               fill = group_macro))
+  
+  p <- p + geom_bar(position = 'dodge', 
+                    stat = 'identity', 
+                      alpha = 0.7)
+  p <- p + coord_flip()
+  # add percentage text to the right
+  p <- p + geom_text(aes(label = round(percent_change, 1)), 
+                       y = max(pd$percent_change)*1.3,
+                       color = 'black', 
+                       size = 4)
+  
+  # set limit 
+  pc_max <- max(pd$percent_change)
+  pc_min <- min(pd$percent_change)
+  p <- p + ylim(pc_min - 0.1*(pc_max - pc_min), 
+                pc_max + 0.1*(pc_max - pc_min))
+  # theme
+  p <- p + theme_minimal()
+  p <- p + theme(panel.grid.major.y = element_blank())
+  p <- p + scale_fill_brewer(palette = 'Dark2')
+  p <- p + labs(title = title_text, 
+                  x = axis_x_text, 
+                  y = axis_y_text)
+  p <- p + theme(axis.text = element_text(size = 12), 
+                   axis.title = element_text(size = 12), 
+                   plot.title = element_text(size = 20), 
+                   legend.position = 'none', 
+                   axis.text.y = element_blank(), 
+                   axis.title.y = element_blank())
+  p
+  
+}
+
+
+
+
+# table contrib ----
+
+# prep_contrib(data_contrib = data_contrib)
+# dtt <- prep_contrib(data_contrib = data_contrib, demo = T)
+
+prep_contrib <- function(data_contrib, demo = F){
+  
+  td <- data_contrib[, c("tag_outcome",
+                         "tc_new_diet", 
+                         "total_contrib_raw", 
+                         "min", 
+                         "max", 
+                         "is_ok",
+                         "dev_if_not_ok")]
+  
+  if(demo == T){
+    # add a fake row for exceeding upper threshold
+    nr <- nrow(td)+1
+    td[nr, ] <- td[2, ]
+    td[nr, ]$tag_outcome <- 'fake_metric'
+    td[nr, ]$tc_new_diet <- 90.21
+    td[nr, ]$is_ok <- 'beyond_upper'
+    td[nr, ]$dev_if_not_ok <- 0.018
+    
+  }
+  
+  td <- data.table::setDT(td)
+  data.table::setnames(td, old = 'tc_new_diet', 'tc_new')
+  data.table::setnames(td, old = 'total_contrib_raw', 'tc_current')
+  td[, min := round(min, digits = 1)]
+  td[, max := round(max, digits = 1)]
+  td[, dev_if_not_ok := dev_if_not_ok*100]
+  td[, checker := is_ok]
+  
+  td[checker == 'Yes', checker := '-']
+  td[checker == 'beyond_lower', checker := paste0(abs(dev_if_not_ok), '% below')]
+  td[checker == 'beyond_upper', checker := paste0(abs(dev_if_not_ok), '% above')]
+  
+  # drop columns
+  td[, is_ok := NULL]
+  td[, dev_if_not_ok := NULL]
+  
+  td <- data.frame(td)
+  result <- list(data_contrib = data_contrib, 
+                 gt_data = td, 
+                 demo = demo)
+  # result <- structure(result, class = 'diet_comparison_percent')
+  return(result)
+}
+
+
+# tab_contrib(tab_obj = dtt)
+
+tab_contrib <- function(tab_obj){
+  
+  td <- tab_obj$gt_data
+  # make gt table
+  gtt <- gt(td)
+  # merge min and max in one col
+  gtt <- cols_merge(gtt, 
+                    columns = c(min, max), 
+                    pattern = "{1}&dash;{2}")
+  
+  # add header
+  gtt <- tab_spanner(gtt, 
+                     label = md("**Total contribution**"), 
+                     columns = c(tc_new, tc_current))
+  
+  gtt <- tab_spanner(gtt, 
+                     label = md("**Constraint**"), 
+                     columns = c(min, checker))
+  # change text
+  gtt <- cols_label(gtt, 
+                    tag_outcome = 'Outcome', 
+                    tc_new = 'New diet', 
+                    tc_current = "Current diet", 
+                    min = "Range", 
+                    checker = 'Comments')
+
+  # center checker column
+  gtt <- cols_align(gtt, align = 'center', columns = checker)
+  
+  # boldface tc new
+  gtt <- tab_style(gtt, 
+                   locations = cells_body(columns = tc_new), 
+                   style = cell_text(weight = 'bold'))
+  # add color: blue for below, red for above
+  gtt <- tab_style(gtt, 
+                   locations = cells_body(
+                     columns = c(tc_new, checker), 
+                     rows = (tc_new < tc_current)&checker != '-'), 
+                   style = cell_fill(color = 'lightblue', alpha = 0.8))
+  
+  gtt <- tab_style(gtt, 
+                   locations = cells_body(
+                     columns = c(tc_new, checker), 
+                     rows = (tc_new > tc_current)&checker != '-'), 
+                   style = cell_fill(color = 'lightcoral', alpha = 0.8))
+  gtt
+  
+  return(gtt)
+}
+
+
+
+
+
+
---
+++ dev/viz_result.R
@@ -0,0 +1,209 @@
+# development code for result visualization 
+
+data_newdiet <- readRDS('./inst/rawdata/demo_12foods_res1.rda')
+data_contrib <- readRDS('./inst/rawdata/demo_12foods_res2.rda')
+
+
+data_newdiet
+data_contrib
+
+
+# need to compare 
+
+library(ggplot2)
+library(gt)
+library(gtExtras)
+
+
+# data manip (put to data-raw) ----
+
+foodname_group
+
+# attach to data_newdiet
+# data_newdiet
+
+newdiet <- dplyr::left_join(data_newdiet, 
+                            foodname_group, 
+                            by = 'food_name')
+
+# new diet ----
+
+# p1: only compare current and new
+name_ordered <- foodname_group$food_name
+
+pd <- select(newdiet, c(food_name, current, new, group_macro))
+pd$food_name_ordered <- factor(pd$food_name, 
+                             levels = name_ordered, 
+                             labels = name_ordered)
+pd
+# summary(pd)
+# pd <- tidyr::pivot_longer(data = newdiet, 
+#                           cols = -c(food_name, group_macro), 
+#                           names_to = 'metric')
+
+
+p1 <- ggplot(data = pd, aes(x = food_name_ordered, y = new, 
+                              fill = group_macro))
+p1 <- p1 + geom_bar(position = 'dodge', stat = 'identity')
+p1 <- p1 + coord_flip()
+p1 <- p1 + geom_text(aes(label = round(new, 1)), 
+                     hjust = -0.8, 
+                     color = 'black', 
+                     size = 4)
+p1 <- p1 + ylim(0, max(pd$new)*1.1)
+p1 <- p1 + geom_errorbar(aes(ymin = current, ymax = current+1), 
+                         width = 1)
+p1 <- p1 + theme_minimal()
+p1 <- p1 + theme(panel.grid.major.y = element_blank())
+p1 <- p1 + scale_fill_brewer(palette = 'Dark2')
+p1 <- p1 + labs(title = 'New diet', 
+                x = 'Food groups', 
+                y = 'Intake (grams)')
+p1 <- p1 + theme(axis.text = element_text(size = 12), 
+                 axis.title = element_text(size = 12), 
+                 axis.title.y = element_blank(),
+                 plot.title = element_text(size = 20), 
+                 legend.position = 'none')
+
+p1
+
+
+
+
+# p2 ----
+
+name_ordered <- foodname_group$food_name
+
+pd2 <- select(newdiet, c(food_name, group_macro, percent_change))
+pd2$food_name_ordered <- factor(pd2$food_name, 
+                               levels = name_ordered, 
+                               labels = name_ordered)
+
+
+p2 <- ggplot(data = pd2, aes(x = food_name_ordered, 
+                             y = percent_change, 
+                             fill = group_macro))
+
+p2 <- p2 + geom_bar(position = 'dodge', stat = 'identity', 
+                    alpha = 0.7)
+p2 <- p2 + coord_flip()
+p2 <- p2 + geom_text(aes(label = round(percent_change, 1)), 
+                     y = max(pd2$percent_change)*1.3,
+                     color = 'black', 
+                     size = 4)
+p2 <- p2 + ylim(min(pd2$percent_change) - abs(0.1*(pd2$percent_change)), 
+                max(pd2$percent_change)*1.3)
+
+p2 <- p2 + theme_minimal()
+p2 <- p2 + theme(panel.grid.major.y = element_blank())
+p2 <- p2 + scale_fill_brewer(palette = 'Dark2')
+p2 <- p2 + labs(title = 'Change by percent', 
+                x = 'Food groups', 
+                y = 'Percent change ')
+p2 <- p2 + theme(axis.text = element_text(size = 12), 
+                 axis.title = element_text(size = 12), 
+                 plot.title = element_text(size = 20), 
+                 legend.position = 'none', 
+                 axis.text.y = element_blank(), 
+                 axis.title.y = element_blank())
+p2
+
+# library(patchwork)
+p1+p2+plot_layout(nrow = 1)
+
+
+
+
+
+
+
+
+
+# validation -----
+
+data_contrib
+
+# reorder
+td <- data_contrib[, c("tag_outcome",
+                       "tc_new_diet", 
+                       "total_contrib_raw", 
+                       "min", 
+                       "max", 
+                       "is_ok",
+                       "dev_if_not_ok")]
+
+# add a fake row for exceeding upper threshold
+td[nrow(td)+1, ] <- td[2, ]
+td[nrow(td)+1, ]$tag_outcome <- 'fake_metric'
+td[nrow(td)+1, ]$tc_new_diet <- 90.21
+td[nrow(td)+1, ]$is_ok <- 'beyond_upper'
+td[nrow(td)+1, ]$dev_if_not_ok <- 0.018
+
+
+td <- data.table::setDT(td)
+data.table::setnames(td, old = 'tc_new_diet', 'tc_new')
+data.table::setnames(td, old = 'total_contrib_raw', 'tc_current')
+td[, min := round(min, digits = 1)]
+td[, max := round(max, digits = 1)]
+td[, dev_if_not_ok := dev_if_not_ok*100]
+td[, checker := is_ok]
+
+td[checker == 'Yes', checker := '-']
+td[checker == 'beyond_lower', checker := paste0(abs(dev_if_not_ok), '% below')]
+td[checker == 'beyond_upper', checker := paste0(abs(dev_if_not_ok), '% above')]
+
+# drop columns
+td[, is_ok := NULL]
+td[, dev_if_not_ok := NULL]
+
+td
+
+
+# make table
+
+gtt <- gt(td)
+gtt <- cols_merge(gtt, 
+                  columns = c(min, max), 
+                  pattern = "{1}&dash;{2}")
+
+gtt <- tab_spanner(gtt, 
+                   label = md("**Total contribution**"), 
+                   columns = c(tc_new, tc_current))
+
+gtt <- tab_spanner(gtt, 
+                   label = md("**Constraint**"), 
+                   columns = c(min, checker))
+
+gtt <- cols_label(gtt, 
+                  tag_outcome = 'Outcome', 
+                  tc_new = 'New diet', 
+                  tc_current = "Current diet", 
+                  min = "Range", 
+                  checker = 'Comments')
+gtt
+
+
+
+# put new tc first, and boldface
+# original (min, max)
+gtt <- cols_align(gtt, align = 'center', columns = checker)
+
+gtt <- tab_style(gtt, 
+                 locations = cells_body(columns = tc_new), 
+                 style = cell_text(weight = 'bold'))
+
+gtt <- tab_style(gtt, 
+                 locations = cells_body(
+                   columns = c(tc_new, checker), 
+                   rows = (tc_new < tc_current)&checker != '-'), 
+                 style = cell_fill(color = 'lightblue', alpha = 0.8))
+
+gtt <- tab_style(gtt, 
+                 locations = cells_body(
+                   columns = c(tc_new, checker), 
+                   rows = (tc_new > tc_current)&checker != '-'), 
+                 style = cell_fill(color = 'lightcoral', alpha = 0.8))
+gtt
+
+
+
